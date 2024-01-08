18a9b81797ed00c71383770861fc178405a53a05
Xalamor <155827836+Xalamor@users.noreply.github.com>
2024-01-08 22:50:49 +0300

    Add files via upload

---
+++ js/burger_1.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.button_1').forEach((el) => {
+    el.addEventListener('click', () => {
+    
+    let content = el.nextElementSibling;
+
+    if(content.style.maxHeight) {
+        document.querySelectorAll('.burger_1').forEach((el) => el.style.maxHeight =null)
+    } else {
+        document.querySelectorAll('.burger_1').forEach((el) => el.style.maxHeight =null)
+        content.style.maxHeight = content.scrollHeight + 'px';
+    }
+    })
+})
+
+
+
